493bc71cd4fece5faea37e8109c052ff9bdad7cf
AlexThuander <thuander234@gmail.com>
2020-07-31 18:57:55 -0700

    LaraTok moved

---
+++ app/Models/LaraTokSessionModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LaraTokSessionModel extends Model
+{
+    protected $table = 'laratok_sessions';
+
+    protected $fillable = [
+      'session_name',
+      'sessionId',
+      'media_mode',
+      'archive_mode',
+      'location'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tokens() {
+      return $this->hasMany('LaraTok\LaraTokTokenModel');
+    }
+}
