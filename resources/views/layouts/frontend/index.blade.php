802cb926863661ee0cd6d3f969a7f868df133e16
AlexThuander <thuander234@gmail.com>
2020-08-03 01:44:43 -0700

    Update index.blade.php

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -30,11 +30,9 @@
             </div>
 
             <div class="col-md-2 col-lg-3 col-xl-3 d-none d-md-block">
-                
             </div>
 
             <div class="col-md-2 col-lg-3 col-xl-3 d-none d-md-block">
-                
             </div>
 
             <div class="col-sm-5 col-md-3 col-lg-2 col-xl-2 d-none d-sm-block">
